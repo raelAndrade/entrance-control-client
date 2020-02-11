d509e8a4beb561e59cd62ed1737bfdbe8510bb7c
raelAndrade <israelg.andrade@bol.com.br>
2020-02-11 12:45:10 -0300

    Correção shared

---
+++ src/app/services/SharedService.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  /* public static instance: SharedService = null;
+  user: User;
+  token: string;
+  showTemplate = new EventEmitter<boolean>(); */
+  constructor() {
+    /* return SharedService.instance = SharedService.instance || this; */
+  }
+}
